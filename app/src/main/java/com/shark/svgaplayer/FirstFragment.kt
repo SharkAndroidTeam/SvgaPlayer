c0ceacc247b300ef13b27184adf7b00f3bf8e17c
wushh <wushihao@inke.cn>
2022-05-09 17:12:15 +0800

    [base]简单demo

--- app/src/main/java/com/shark/svgaplayer/FirstFragment.kt
+++ app/src/main/java/com/shark/svgaplayer/FirstFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.shark.svgaplayer.databinding.FragmentFirstBinding
+import com.shark.svgaplayer_base.util.load
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,10 +32,11 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.svgaImg.post {
+            binding.svgaImg.load("https://media.kaiyinhn.cn/MTY1MjA4Njg1Nzk4OCM2Mjkjc3ZnYQ==.svga")
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
     }
 
     override fun onDestroyView() {
