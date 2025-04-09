16fe77260c67c23cca416b54cc59a91bc1164f31
wushh <wush@flatincbr.com>
2025-04-09 14:19:40 +0800

    添加jitpack配置

--- svgaplayer_base/build.gradle.kts
+++ svgaplayer_base/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("maven-publish")
 }
 
 android {
@@ -42,4 +43,17 @@ dependencies {
     api(libs.okhttp)
     api(libs.okio)
     api(libs.wire)
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.shark.svga_player"
+                artifactId = "svga_plaer"
+                version = "1.0.2"
+            }
+        }
+    }
 }
